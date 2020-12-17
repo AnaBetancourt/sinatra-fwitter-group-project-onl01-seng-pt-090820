c4e571d248fc1168a7932ec08f4b0b3e683f84ed
AnaBetancourt <anaa.betancourt13@gmail.com>
2020-12-16 22:01:50 -0500

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,34 @@
 class UsersController < ApplicationController
 
+    get '/signup' do
+        erb :'/users/new'
+    end
+
+    post '/signup' do
+        if params[:username] != "" && params[:password] != "" && params[:email] != ""
+            @user = User.new(params)
+            session[:user_id] = @user.id
+            redirect '/tweets'
+        elsif logged_in?
+            redirect '/tweets'
+        else
+            "I'm sorry, you have entered an invalid input, please try again."
+            redirect '/signup'
+        end
+    end
+    
+    get '/login' do
+        erb :'/users/login'
+    end
+
+    helpers do
+        def logged_in?
+          !!session[:user_id]
+        end
+    
+        def current_user
+          User.find(session[:user_id])
+        end
+    end
 
 end
